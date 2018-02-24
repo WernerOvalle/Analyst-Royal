57b2623dfdd5917c16e1b217e01e70a8f2d11537
Werner Ovalle <32042983+WernerOvalle@users.noreply.github.com>
2018-02-24 16:41:45 -0600

    Add files via upload
    navegador

---
+++ Navegadorfinal/Navegador/Conector.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace Navegador
+{
+
+    public class Conector
+
+    {
+        private MySqlConnection conexion;
+        private string sConexion, server, db, user, pass, SQL;
+
+
+        public Conector()
+        {
+        }
+
+        public Conector(string server, string db, string user, string pass, string SQL)
+        {
+            this.server = server;
+            this.db = db;
+            this.user = user;
+            this.pass = pass;
+            this.SQL = SQL;
+        }
+
+
+
+        public void OpenConnection()
+        {
+
+
+            MessageBox.Show(Globales.SQL);
+            sConexion = "SERVER=" + this.server + ";" + "DATABASE=" +
+                this.db + ";" + "UID=" + this.user + ";" + "PASSWORD=" + this.pass + ";";
+
+
+
+            try
+            {
+                //This is my connection string i have assigned the database file address path  
+                //This is my insert query in which i am taking input from the user through windows forms  
+                //This is  MySqlConnection here i have created the object and pass my connection string.  
+                MySqlConnection MyConn2 = new MySqlConnection(sConexion);
+                //This is command class which will handle the query and connection object.  
+                MySqlCommand MyCommand2 = new MySqlCommand(Globales.SQL, MyConn2);
+                MySqlDataReader MyReader2;
+                MyConn2.Open();
+                MyReader2 = MyCommand2.ExecuteReader();     // Here our query will be executed and data saved into the database.  
+                MessageBox.Show("Exito");
+                while (MyReader2.Read())
+                {
+                }
+                MyConn2.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+
+
+        //Close connection
+        public void CloseConnection()
+        {
+            try
+            {
+                conexion.Close();
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+    }
+}
---
+++ Navegadorfinal/Navegador/Navegador.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Navegador
+{
+
+    public partial class Navegador : UserControl
+    {
+
+
+        [Description("Direccion servidor")]
+        [Category("ParametrosBD")]
+        public String sServidor { get; set; }
+        [Description("BaseDatos")]
+        [Category("ParametrosBD")]
+        public String sNombreBD { get; set; }
+        [Description("NombreTabla")]
+        [Category("ParametrosBD")]
+        public String sNombreTabla { get; set; }
+        [Description("Usuario")]
+        [Category("ParametrosBD")]
+        public String sUsuario { get; set; }
+        [Description("Contraseña")]
+        [Category("ParametrosBD")]
+        public String sPass { get; set; }
+        private Conector con;
+        private INSERCION insert;
+        private ACTUALIZAR update;
+        private ELIMINAR delete;
+
+
+
+
+
+        public event EventHandler NavInsertar;
+        public event EventHandler NavActualizar;
+        public event EventHandler NavEliminar;
+        public event EventHandler NavGuardar;
+        public event EventHandler NavSiguiente;
+        public event EventHandler NavAnterior;
+        public event EventHandler NavPrimero;
+        public event EventHandler NavUltimo;
+
+        public Navegador()
+        {
+            InitializeComponent();
+
+        }
+
+
+       
+
+
+
+
+
+
+
+       private void button6_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            insert = new INSERCION(sNombreTabla);
+            insert.empezandosql();
+            if (this.NavInsertar != null)
+                this.NavInsertar(this, e);
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button3.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+
+            update = new ACTUALIZAR(sNombreTabla);
+            update.empezandosql();
+            if (this.NavActualizar != null)
+                this.NavActualizar(this, e);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.NavGuardar != null)
+                this.NavGuardar(this, e);
+
+            con = new Conector(sServidor, sNombreBD, sUsuario, sPass, Globales.SQL);
+            con.OpenConnection();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            delete = new ELIMINAR(sNombreTabla);
+            delete.empezandosql();
+            if (this.NavEliminar != null)
+                this.NavEliminar(this, e);
+
+        }
+
+       
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+        }
+    }
+}
+
+
+
